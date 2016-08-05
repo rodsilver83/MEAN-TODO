825bb168baca8e1abd4996de3db9bed03c2c6082
Rodrigo Soria Dosal <rodsilver83@hotmail.com>
2016-08-05 07:29:30 -0500

    change app to public

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      'app/scripts/**/*.js',
+      '../public/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
